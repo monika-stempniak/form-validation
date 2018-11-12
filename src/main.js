e510c8fd15fe056daefe700eb0d81eab6b9b8af9
monika-stempniak <mstempniak@gmail.com>
2018-11-12 12:31:07 +0100

    Update html form and develop form validation

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 require("./style.scss");
 
 const required = (value) => {
-  console.log("val", value);
   const isValue = typeof(value) === "boolean" ? value : Boolean(value.length);
-  console.log("isValue", isValue);
   return {
     isValid: isValue,
-    errorMessages: isValue ? null : 'This field is required'
+    errorMessage: isValue ? "" : "This field is required."
+  }
+}
+
+const checkLength = (value, requiredLength = 3) => {
+  const isLongEnough = value.length > requiredLength;
+  const char = requiredLength <= 1 ? "character" : "characters";
+  return {
+    isValid: isLongEnough,
+    errorMessage: isLongEnough ? "" : `Text needs to be at least ${requiredLength} ${char} long.`
+  }
+}
+
+const checkEmail = (value) => {
+  const isEmailValid = value.indexOf("@") !== -1;
+  return {
+    isValid: isEmailValid,
+    errorMessage: isEmailValid ? "" : "This should be a valid email."
   }
 }
 
@@ -24,6 +39,8 @@ class FormValidator {
     const tagReference = (tagName, type) => 
       tagName === "INPUT" ? this.form.querySelectorAll(`${tagName}[type=${type}]`) : this.form.querySelectorAll(`${tagName}`);
 
+    const errorReference = (name) => this.form.querySelector(`.${name}-error`);
+
     Array.from(formElements).forEach(element => {
       const {tagName, type, name, value, checked} = element;
       if (tags.indexOf(tagName) !== -1) {
@@ -33,14 +50,29 @@ class FormValidator {
           reference: tagReference(tagName, type),
           isValid: true,
           validators: null,
-          errorMessages: null,
+          errorMessages: [],
+          errorFieldReference: errorReference(name),
         }
       }
     });
   }
   addEventListeners() {
+    this.form.addEventListener("submit", e => {
+      e.preventDefault();
+      console.log("validate");
+      // this.addEventListeners();
+      Object.values(this.virtualForm).forEach(element => {
+        Array.from(element.reference).forEach(item => {
+          // console.log(element);
+          this.formValidate(element, item);
+        });
+      });
+    });
+
     Object.values(this.virtualForm).forEach(element => {
         Array.from(element.reference).forEach(item => {
+          // formValidate(e);
+          // console.log(item);
           item.addEventListener("blur", e => {
             const {name, type, checked, value} = e.target;
             const formElement = this.virtualForm[name];
@@ -51,30 +83,42 @@ class FormValidator {
               formElement.value = value;
               console.log(this.virtualForm);
             }
-
-            if (formElement.validators) {
-              formElement.validators.forEach(validator => {
-                const validation = validator(formElement.value);
-                Object.assign(formElement, validation);
-                console.log(this.virtualForm);
-                if (!validation.isValid) {
-                  item.classList.add("is-not-valid");
-                } else {
-                  item.classList.remove("is-not-valid");
-                }
-              });
-            }
-
+            this.formValidate(formElement, item);
           });
         })
     });
 
-    // this.form.addEventListener("submit", e => {
-    //   e.preventDefault();
-    //   console.log("validate");
-    //   this.formValidate();
-    // });
+
+
   }
+
+  formValidate(formElement, item) {
+    if (formElement.validators) {
+      let errorMessages = [];
+      formElement.validators.forEach(validator => {
+        const validation = validator(formElement.value);
+        errorMessages.push(validation.errorMessage);
+        console.log(errorMessages);
+        Object.assign(formElement, {
+          isValid: validation.isValid,
+          errorMessages,
+        });
+        let message = "";
+        formElement.errorMessages.forEach(msg => {
+          message = message + " " + msg.trim();
+          return message;
+        });
+        console.log(message);
+        formElement.errorFieldReference.textContent = message;
+        if (!validation.isValid) {
+          item.classList.add("is-not-valid");
+        } else {
+          item.classList.remove("is-not-valid");
+        }
+      });
+    }
+  }
+
   setValidators(validators) {
     Object.keys(validators).forEach(key => {
       if(!this.virtualForm[key]) return; 
@@ -85,24 +129,15 @@ class FormValidator {
     });
     console.log(this.virtualForm);
   }
-  // formValidate() {
-  //   Object.values(this.virtualForm).forEach(element => {
-  //     Array.from(element.validators).forEach(item => {
-  //       console.log(item);
-  //     });
-  //   });
-  // }
 }
 
 const form = new FormValidator(document.querySelector('form'));
 
 form.setValidators({
-  username: [required],
-  email: [required],
+  username: [required, checkLength],
+  email: [required, checkEmail],
   gender: [required],
   rodo: [required],
   regions: [required],
   remark: [required],
 })
-
-// form.createVirtualForm();
